fdb5edce82e684cb10f9aeaf303ab52e9b8e1052
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-03-06 17:43:22 -0300

    tests

--- src/lib/trigram.ts
+++ src/lib/trigram.ts
@@ -7,19 +7,28 @@ export function isAlphanumeric(code: number) {
 }
 
 function generateWords(text: string) {
-  const words = new Set<string>();
+  const words: Array<string> = [];
 
   let start = 0,
     end = 0;
 
   for (let i = 0; i < text.length; i++) {
     if (i === text.length - 1) {
-      words.add(text.slice(start, end + 1));
+      const word = text.slice(start, end + 1);
+      if (!words.includes(word)) {
+        words.push(word);
+      }
       return words;
     }
 
     if (!isAlphanumeric(text.charCodeAt(i))) {
-      words.add(text.slice(start, end));
+      if (start !== end) {
+        const word = text.slice(start, end);
+        if (!words.includes(word)) {
+          words.push(text.slice(start, end));
+        }
+      }
+
       start = end + 1;
     }
 
@@ -36,18 +45,18 @@ function filterLetters(word: string) {
 export function trigram(text: string) {
   const words = generateWords(text.toLocaleLowerCase());
 
-  if (words.size === 0) return [];
+  if (words.length === 0) return [];
 
   const trigrams = new Set();
 
   const prefix_len = 2;
   const suffix_len = 1;
 
-  for (const word of words) {
+  for (let i = 0; i < words.length; i++) {
+    const word = words.at(i) || "";
+
     const letters = filterLetters(word);
-    if (letters.length === 0) {
-      continue;
-    }
+    if (letters.length === 0) continue;
 
     for (let i = -prefix_len; i < letters.length - suffix_len; i++) {
       const len = word.length;
